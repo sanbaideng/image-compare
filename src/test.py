c6fac81b4356af566d9261df6e7d5a239bc2482f
yangyang <yangyang@cecii.cn>
2020-12-14 13:30:37 +0800

    删除无用代码

--- src/test.py
+++ src/test.py
@@ -12,37 +12,6 @@ from comAlth import aHash
 from comAlth.aHash import classfiy_aHash
 from comAlth.histogram2 import *
 
-def compare(pic1,pic2):
-    '''
-    :param pic1: 图片1路径
-    :param pic2: 图片2路径
-    :return: 返回对比的结果
-    '''
-    # image1 = Image.open(pic1)
-    # image2 = Image.open(pic2)
-
-    # histogram1 = image1.histogram()
-    # histogram2 = image2.histogram()
-
-    # differ = math.sqrt(reduce(operator.add, list(map(lambda a,b: (a-b)**2,histogram1, histogram2)))/len(histogram1))
-
-
-    #print(differ)
-    differ = PHash.score(pic1,pic2)
-    return differ
-
-def compareHist(stdimg, ocimg):
-    stdimg = cv2.imread(str(stdimg), 0)
-    ocimg = cv2.imread(str(ocimg), 0)
-    stdimg = np.float32(stdimg)
-    ocimg = np.float32(ocimg)
-    stdimg = np.ndarray.flatten(stdimg)
-    ocimg = np.ndarray.flatten(ocimg)
-    imgocr = np.corrcoef(stdimg, ocimg)
-    if imgocr[0, 1] > 0.6:
-        print("imgocr",imgocr[0, 1])
-    return imgocr[0, 1] > 0.90
-
 g1 = os.walk(r"xjcy")
 g2 = os.walk(r'muyu')
 file_list1 = []
@@ -83,23 +52,20 @@ if __name__ == '__main__':
     dic[file2_len] = file1_len
     print(dic)
     for idx, file2 in enumerate(file_list2):
-        index=current+1
-        while index<file1_len:
+        index = current+1
+        while index < file1_len:
             file1 = file_list1[index-1]
-            #二分法
-            if idx/file2_len*2 <index/file1_len:
+            # 二分法
+            if idx/file2_len*2 < index/file1_len:
                 break
-            if(compareIH(file1,file2,4)):
+            if(compareIH(file1, file2, 4)):
                 print("找到相近的图片：", idx, file2, file1, index, current)
                 current = index
                 break
-            index+=1
+            index += 1
 
-def BinarySearch(dict,low,high,pic):
 
-    return dict
-
-def findSimiPic(file_list1,file_list2,dict,index,low,high):
+def findSimiPic(file_list1, file_list2, dict, index, low, high):
     file1_len = len(file_list1)
     file2_len = len(file_list2)
     file1_low = dict[low]
@@ -109,17 +75,18 @@ def findSimiPic(file_list1,file_list2,dict,index,low,high):
     while file1_index < file1_high:
         file1 = file_list1[index-1]
 
-        if idx/file2_len*2 <index/file1_len:
+        if idx/file2_len*2 < index/file1_len:
             break
-        if(compareIH(file1,file2,4)):
+        if(compareIH(file1, file2, 4)):
             print("找到相近的图片：", file2, file1, file1_index)
-            dict[index]=file1_index
+            dict[index] = file1_index
             return dict
-        file1_index+=1
-    dict[index]=-2
+        file1_index += 1
+    dict[index] = -2
     return dict
 
-def findSimilarPic(file_list1,file_list2,dict,index, low,high):
+
+def findSimilarPic(file_list1, file_list2, dict, index, low, high):
     file1_len = len(file_list1)
     file2_len = len(file_list2)
     file1_low = dict[low]
@@ -128,12 +95,10 @@ def findSimilarPic(file_list1,file_list2,dict,index, low,high):
     dict = findSimiPic(file_list1, file_list2, dict, index, low, high)
     plus = True
     count = 1
-    if(dict[index]==-2):
-        count+=count
+    if(dict[index] == -2):
+        count += count
         if(plus):
             findSimiPic(file_list1, file_list2, dict, index+count, low, high)
         else:
             findSimiPic(file_list1, file_list2, dict, index - count, low, high)
         plus = not plus
-
-
